85052ce030a870ec9661734d0dbeafb5761fbce8
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2025-06-23 19:38:13 +0700

    fix image in categories show blade

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -16,7 +16,7 @@
                             </button>
                         </div>
                         <div class="col-md-4 my-auto p-0">
-                            <img src="{{ Storage::url($category->image) }}"
+                            <img src="{{($category->image) }}"
                                 class="img-fluid img-jumbotron d-none d-md-block" />
                         </div>
                     </div>
@@ -32,7 +32,7 @@
                 @foreach ($category->menus as $menu)
                     <div class="col-md-3">
                         <div class="card card-borderless-shadow card-min-height">
-                            <img src="{{ Storage::url($menu->image) }}"
+                            <img src="{{($menu->image) }}"
                                 class="card-img-top card-img-top-landing-page" />
                             <div class="card-body">
                                 <h5 class="card-title fw-bold"> {{ $menu->name }}</h5>
