1d82cabfe784d2def57225a6e7564ab9ad0d7d09
namanyafaris <farisg489@gmail.com>
2025-06-25 11:39:12 +0700

    loose storage:url

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -16,7 +16,7 @@
                             </button>
                         </div>
                         <div class="col-md-4 my-auto p-0">
-                            <img src="{{ Storage::url($category->image) }}"
+                            <img src="{{ ($category->image) }}"
                                 class="img-fluid img-jumbotron d-none d-md-block" />
                         </div>
                     </div>
@@ -30,20 +30,20 @@
         <div class="container mt-4 mb-5">
             <div class="row g-3">
                 @foreach ($category->menus as $menu)
-                    <div class="col-md-3">
-                        <div class="card card-borderless-shadow card-min-height">
-                            <img src="{{ Storage::url($menu->image) }}"
-                                class="card-img-top card-img-top-landing-page" />
-                            <div class="card-body">
-                                <h5 class="card-title fw-bold"> {{ $menu->name }}</h5>
-                                <hr>
-                                <h5 class="fw-semibold">Rp.{{ $menu->price }}.000,00</h5>
-                                <a href="{{ route('menus.show', $menu->id) }}" class="btn btn-warning w-100 mt-2 fw-semibold">
-        Lihat Detail
-    </a>
-                            </div>
+                <div class="col-md-3">
+                    <div class="card card-borderless-shadow card-min-height">
+                        <img src="{{ ($menu->image) }}"
+                            class="card-img-top card-img-top-landing-page" />
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold"> {{ $menu->name }}</h5>
+                            <hr>
+                            <h5 class="fw-semibold">Rp.{{ $menu->price }},00</h5>
+                            <a href="{{ route('menus.show', $menu->id) }}" class="btn btn-warning w-100 mt-2 fw-semibold">
+                                Lihat Detail
+                            </a>
                         </div>
                     </div>
+                </div>
                 @endforeach
             </div>
         </div>
